1217de7c144337e1e42d367a7968cc4ad57adfce
YanikRijgersberg <9024941@student.zadkine.nl>
2024-11-04 18:07:17 +0000

    piem

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function search() {
     const query = document.getElementById('searchInput').value;
-    if (query === 'index') {
-        window.location.href = 'index.html'; // Redirect naar index.html
+    if (query === 'home') {
+        window.location.href = 'index.html';
     } else if (query === 'canvas') {
-        window.location.href = 'canvas.html'; // Redirect naar canvas.html
-    } else if (query) {
-        window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+        window.location.href = 'canvas.html';
+    } else if (query==='faciliteiten') {
+        window.location.href = 'faciliteiten.html' 
     }
 }
 
